5b72e1db64a5dbaebf95ab373211a17c7f7b7a40
Lucas Amaral <amarall.tx@gmail.com>
2025-07-25 20:33:21 -0300

    Add gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// src/App.jsx
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./containers/layout/Layout";
@@ -6,21 +5,26 @@ import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
 import ExplorePage from "./pages/ExplorePage";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
     {
-        path: "/",
-        element: <Layout />,
-        children: [
-            { path: "/", element: <HomePage /> },
-            { path: "/explore", element: <ExplorePage /> },
-            { path: "/species", element: <div>Espécies</div> },
-            { path: "/about", element: <div>Sobre</div> },
-            {path: "/discover", element: <div>Conhecer o passado para proteger o futuro.</div>,},
-            { path: "*", element: <NotFoundPage /> },
-        ],
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "/", element: <HomePage /> },
+        { path: "/explore", element: <ExplorePage /> },
+        { path: "/species", element: <div>Espécies</div> },
+        { path: "/about", element: <div>Sobre</div> },
+        { path: "/discover", element: <div>Conhecer o passado para proteger o futuro.</div> },
+        { path: "*", element: <NotFoundPage /> },
+      ],
     },
-]);
+  ],
+  {
+    basename: "/Mymba/",
+  }
+);
 
 export default function App() {
-    return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 }
